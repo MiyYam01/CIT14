4e3839a681f3f6da6d6c37b51578d623b4df4da7
CIT142F-28@M307.UC-BCF.EDU.PH <CIT142F-28@M307.UC-BCF.EDU.PH>
2022-09-21 17:51:18 +0800

    Batch File

--- Push - GitHub.bat
+++ Push - GitHub.bat
@@ -1,6 +1,6 @@
 @echo off
 
-git remote set-url origin https://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@github.com/MiyYam01/CIT14.git
+git remote set-url origin https://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@github.com/MiyYam01/CIT14.git
 
 git add .
 git commit -m "Batch File"
